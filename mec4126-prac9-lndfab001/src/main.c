91b451e94892780c025c855dbb361ed6b2394c8b
Fabian <LNDFAB001@myuct.ac.za>
2021-06-15 15:29:30 +0200

    Q2: Interrupt generated every 50ms by timer 6.

--- mec4126-prac9-lndfab001/src/main.c
+++ mec4126-prac9-lndfab001/src/main.c
@@ -19,7 +19,11 @@
 
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
+
 void init_timer_2(void);
+void init_timer_6(void);
+void TIM6_IRQHandler(void);
+
 void main(void);                                                   //COMPULSORY
 void init_ADC(void);											   //COMPULSORY
 #ifdef TRUESTUDIO												   //COMPULSORY
@@ -37,7 +41,8 @@ void main(void)
 #endif															   //COMPULSORY
 
 	init_timer_2();
-
+	init_timer_6();
+	TIM6_IRQHandler();
 	while(1)
 	{
 
@@ -70,21 +75,23 @@ void reset_clock_to_48Mhz(void)									   //COMPULSORY
 
 void init_ADC(void)
 {
-	//
+	//enable port A
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-	//
+	//enable PA6 to analogue mode
 	GPIOA->MODER |= GPIO_MODER_MODER6;
-	//
+	//enable clock to the ADC
 	RCC->APB2ENR |= RCC_APB2ENR_ADCEN;
-	//
+	//Enable ADC
 	ADC1->CR |= ADC_CR_ADEN;
-	//
+	//setup single shot mode
+	ADC1->CFGR1 &= ~ADC_CFGR1_CONT;
+	//Select channel 6
 	ADC1->CHSELR |= ADC_CHSELR_CHSEL6;
-	// Setup in wait mode
+	//Setup in wait mode
 	ADC1->CFGR1 |= ADC_CFGR1_WAIT;
-	// Setup 10 bit resolution
-
-	//
+	//Setup 10 bit resolution
+	ADC1->CFGR1 |= ADC_CFGR1_RES_1;
+	//Wait for ADRDY flag to go high
 	while((ADC1->ISR & ADC_ISR_ADRDY) == 0);
 }
 
@@ -93,25 +100,33 @@ void init_timer_2(void)
 	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;			//clock to timer
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;			//enable clock to PB
 	GPIOB->MODER |= GPIO_MODER_MODER10_1;		//set B10 to alternate functions
-	GPIOB->AFR[1] |= 0b1000000000;				//set AFR to AFR2 for B10
+	GPIOB->AFR[1] |= 0b1000000000;				//set AFR to AFR2 for PB10
 
 	//set up for 15khz
-	TIM2->PSC = 0;
-	TIM2->ARR = 3200;
+	TIM2->PSC = 3;
+	TIM2->ARR = 1023;
 	//configure PWM
 	TIM2->CCMR2 |= TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3PE;
 	//enable CC output
 	TIM2->CCER |= TIM_CCER_CC3E;
 	//enable timer
 	TIM2->CR1 |= TIM_CR1_CEN;
-	//Assigning duty cycle 25%
-	TIM2->CCR3 = 3200 / 4;
-
+	//Assigning duty cycle 100%
+	TIM2->CCR3 = 1023;
 
 }
 
 void init_timer_6(void)
 {
+	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;			//enable clock for timer 6
+
+	//50s delay overflow
+	TIM6->PSC = 37;
+	TIM6->ARR = 64865;
+
+	TIM6->DIER |= TIM_DIER_UIE;					// enable interrupt
+	NVIC_EnableIRQ(TIM6_DAC_IRQn);				// unmask TIM6 interrupt in NVIC
+	TIM6->CR1 |= TIM_CR1_CEN;     				// enable Timer 6
 
 }
 
@@ -120,4 +135,7 @@ void init_timer_6(void)
 void TIM6_IRQHandler(void)
 {
 	TIM6->SR &= ~TIM_SR_UIF;	//acknowledge interrupt
+	ADC1->CR |= ADC_CR_ADSTART; //Start conversion
+	while(((ADC1 -> ISR) &  ADC_ISR_EOC) == 0 ); //conversion complete with EOC flag in ISR goes high
+	TIM2->CCR3 = ADC1->DR; //Save ADC value to TIM2
 }
